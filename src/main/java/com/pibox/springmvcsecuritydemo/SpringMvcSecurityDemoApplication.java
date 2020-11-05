ec2508851a927ed262f032e396294d8e7a4bbbbb
Valentin Piho <val.piho@gmail.com>
2020-11-05 19:02:32 +0200

    Initial commit

---
+++ src/main/java/com/pibox/springmvcsecuritydemo/SpringMvcSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pibox.springmvcsecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSecurityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSecurityDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pibox/springmvcsecuritydemo/SpringMvcSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pibox.springmvcsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
