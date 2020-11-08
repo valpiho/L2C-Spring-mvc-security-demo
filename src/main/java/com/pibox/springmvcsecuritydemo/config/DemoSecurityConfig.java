3075067ef6445fdd1bd8ba96014e8ffab10a4c69
Valentin Piho <val.piho@gmail.com>
2020-11-08 12:50:58 +0200

    Login page

--- src/main/java/com/pibox/springmvcsecuritydemo/config/DemoSecurityConfig.java
+++ src/main/java/com/pibox/springmvcsecuritydemo/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.pibox.springmvcsecuritydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -26,4 +27,14 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new InMemoryUserDetailsManager(user);
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+					.loginPage("/login")
+					.loginProcessingUrl("/authenticateUser")
+					.permitAll();
+	}
 }
