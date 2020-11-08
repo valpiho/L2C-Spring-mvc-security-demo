c0aa18396c70a88e2a0884a816f804613c7fcf55
Valentin Piho <val.piho@gmail.com>
2020-11-08 11:22:28 +0200

    Updated configurations

---
+++ src/main/java/com/pibox/springmvcsecuritydemo/config/WebAppConfig.java
@@ -0,0 +1,25 @@
+package com.pibox.springmvcsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.pibox.springmvcsecuritydemo")
+public class WebAppConfig {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+}
